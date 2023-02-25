ee01fea385d73a8ba5a75463308a6515616d85c0
jkka777 <abhiram.kishore@gmail.com>
2023-02-25 20:34:51 +0530

    modified allowed origins to app services

--- angadi/angadi/src/main/java/com/angadi/configuration/SecurityConfig.java
+++ angadi/angadi/src/main/java/com/angadi/configuration/SecurityConfig.java
@@ -9,7 +9,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -27,7 +29,7 @@ public class SecurityConfig {
 
                     CorsConfiguration cfg = new CorsConfiguration();
 
-                    cfg.setAllowedOrigins(Collections.singletonList("*"));
+                    cfg.setAllowedOrigins(Collections.singletonList("https://prd-project-e-commerce-production.up.railway.app/"));
                     cfg.setAllowedMethods(Collections.singletonList("*"));
                     cfg.setAllowCredentials(true);
                     cfg.setAllowedHeaders(Collections.singletonList("*"));
